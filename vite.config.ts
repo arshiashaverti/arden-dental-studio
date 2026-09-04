60210f09d2ec492e6122a62c9c29329b17325235
Arshia Shaverti <91316681+arshiashaverti@users.noreply.github.com>
2026-09-05 00:37:04 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/arden-dental-studio/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
